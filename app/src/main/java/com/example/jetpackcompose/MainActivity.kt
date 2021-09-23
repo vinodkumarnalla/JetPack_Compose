87757bacdb612a31f726245501784fc5c832a8d5
vinodkumar.nalla <vinodkumar.nalla67@gmail.com>
2021-09-23 12:54:22 +0530

    Added JetPack Compose Library and created simple text and edit text fields

--- app/src/main/java/com/example/jetpackcompose/MainActivity.kt
+++ app/src/main/java/com/example/jetpackcompose/MainActivity.kt
@@ -2,10 +2,51 @@ package com.example.jetpackcompose
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.material.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.unit.dp
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContent(){
+             MaterialTheme() {
+                 Row(modifier = Modifier.fillMaxHeight(), verticalAlignment = Alignment.CenterVertically, horizontalArrangement = Arrangement.Center) {
+                     Greeting(name = "Android")
+                     InputText(name = getString(R.string.app_name))
+                 }
+             }
+
+        }
+    }
+
+    /*Simple function to display text with padding and color*/
+    @Composable
+    fun Greeting(name: String) {
+        Text(text = "Hello $name!", modifier = Modifier.padding(10.dp),style = TextStyle(color = Color.Red))
+    }
+
+    /*Simple function to display edit text*/
+    @Composable
+    fun InputText(name: String) {
+        val textState = remember { mutableStateOf(TextFieldValue()) }
+        TextField(
+            value = textState.value,
+            onValueChange = { textState.value = it },
+            placeholder = { Text(text = name)}
+        )
     }
 }
