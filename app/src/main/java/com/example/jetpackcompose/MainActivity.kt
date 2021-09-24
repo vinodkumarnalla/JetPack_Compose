7d50732688354d594ddfef987c80bbb7159392bd
vinodkumar.nalla <vinodkumar.nalla67@gmail.com>
2021-09-24 14:40:06 +0530

    Added recycler view

--- app/src/main/java/com/example/jetpackcompose/MainActivity.kt
+++ app/src/main/java/com/example/jetpackcompose/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.widget.Toast
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.material.TextField
@@ -24,9 +25,10 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContent(){
              MaterialTheme() {
-                 Row(modifier = Modifier.fillMaxHeight(), verticalAlignment = Alignment.CenterVertically, horizontalArrangement = Arrangement.Center) {
+                 Column(modifier = Modifier.fillMaxHeight()) {
                      Greeting(name = "Android")
                      InputText(name = getString(R.string.app_name))
+                     AddRecyclerView()
                  }
              }
 
@@ -48,5 +50,16 @@ class MainActivity : AppCompatActivity() {
             onValueChange = { textState.value = it },
             placeholder = { Text(text = name)}
         )
+    }
+
+    /*Simple Recycler view*/
+    @Composable
+    fun AddRecyclerView(){
+        val data = arrayOf("Item 1","Item 2","Item 3") 
+        LazyColumn(modifier = Modifier.padding(10.dp)){
+            items(data.size){
+                    Text(text = data[it])
+            }
+        }
     }
 }
