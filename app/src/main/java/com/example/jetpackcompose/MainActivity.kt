1a2fe35f85704677e45e7feb187e2ac63c05c3bc
vinodkumar.nalla <vinodkumar.nalla67@gmail.com>
2021-09-30 19:32:22 +0530

    Added recycler view with multiple views

--- app/src/main/java/com/example/jetpackcompose/MainActivity.kt
+++ app/src/main/java/com/example/jetpackcompose/MainActivity.kt
@@ -1,36 +1,41 @@
 package com.example.jetpackcompose
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Toast
 import androidx.activity.compose.setContent
+import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.Divider
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.material.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.*
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent(){
-             MaterialTheme() {
-                 Column(modifier = Modifier.fillMaxHeight()) {
-                     Greeting(name = "Android")
-                     InputText(name = getString(R.string.app_name))
-                     AddRecyclerView()
-                 }
-             }
+        setContent {
+            MaterialTheme {
+                Column(modifier = Modifier.fillMaxHeight()) {
+                    Greeting(name = "Android")
+                    InputText(name = getString(R.string.app_name))
+                    SimpleRecyclerView()
+                    ComplexRecyclerView()
+                }
+            }
 
         }
     }
@@ -38,7 +43,11 @@ class MainActivity : AppCompatActivity() {
     /*Simple function to display text with padding and color*/
     @Composable
     fun Greeting(name: String) {
-        Text(text = "Hello $name!", modifier = Modifier.padding(10.dp),style = TextStyle(color = Color.Red))
+        Text(
+            text = "Hello $name!",
+            modifier = Modifier.padding(10.dp),
+            style = TextStyle(color = Color.Red)
+        )
     }
 
     /*Simple function to display edit text*/
@@ -48,18 +57,59 @@ class MainActivity : AppCompatActivity() {
         TextField(
             value = textState.value,
             onValueChange = { textState.value = it },
-            placeholder = { Text(text = name)}
+            placeholder = { Text(text = name) }
         )
     }
 
     /*Simple Recycler view*/
     @Composable
-    fun AddRecyclerView(){
-        val data = arrayOf("Item 1","Item 2","Item 3") 
-        LazyColumn(modifier = Modifier.padding(10.dp)){
-            items(data.size){
-                    Text(text = data[it])
+    fun SimpleRecyclerView() {
+        val data = arrayOf("Item 1", "Item 2", "Item 3")
+        LazyColumn(modifier = Modifier.padding(10.dp)) {
+            items(data.size) {
+                Text(text = data[it])
             }
         }
     }
+
+
+    /*Recyclerview with multiple views*/
+    @Composable
+    fun ComplexRecyclerView() {
+        val data = arrayOf("Item 1", "Item 2", "Item 3")
+        LazyColumn(modifier = Modifier.padding(5.dp)) {
+            items(data.size) { count ->
+                Row(modifier = Modifier.padding(5.dp)) {
+                    val image = painterResource(R.drawable.dog)
+                    Image(
+                        painter = image,
+                        contentDescription = "Sample",
+                        modifier = Modifier
+                            .requiredSize(48.dp)
+                            .clip(CircleShape)
+
+                    )
+                    Text(
+                        buildAnnotatedString {
+                            withStyle(style = ParagraphStyle(lineHeight = 20.sp)) {
+                                withStyle(style = SpanStyle(color = Color.Blue)) {
+                                    append("This is an example for JetPack compose recycler view\n")
+                                }
+                                withStyle(
+                                    style = SpanStyle(
+                                        fontWeight = FontWeight.Bold,
+                                        color = Color.Red
+                                    )
+                                ) {
+                                    append(data[count])
+                                }
+                            }
+                        })
+                }
+                Divider()
+            }
+        }
+    }
+
+
 }
